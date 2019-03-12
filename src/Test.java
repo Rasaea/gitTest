73bfb470164e94985c983484821fd9409549aba9
Rasaea <jiwook0819@gmail.com>
2019-03-12 20:42:08 +0900

    첫번째 커밋

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	private static int a;
+	
+	public Test() {
+		a = 1;
+	}
+	
+	public static void main(String []args) {
+		Test t = new Test();
+		System.out.println(a);
+	}
+
+}
